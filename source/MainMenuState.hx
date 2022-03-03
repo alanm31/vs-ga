00dcb3613adc79d5e5fbf117f7d490ccdb7c8f9b
alanm31 <83750874+alanm31@users.noreply.github.com>
2022-03-02 18:54:12 -0800

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -95,7 +95,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
 			//menuItem.screenCenter(X);
-			menuItem.x += 250;
+			menuItem.x += 300;
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
